2d28cabb086a93b661d3adeda8c68a72614ef0e4
sunando bhattacharya <sunandobhattacharya1994@gmail.com>
2017-10-08 06:15:59 +0530

    comit v1.1 - demo page updated.

--- Scripts/app.js
+++ Scripts/app.js
@@ -4,6 +4,12 @@
 /*app.js*/
 
 function upload() {
+	$( "#contentpanel" ).animate({opacity:0}, 1000);
+	$('.progressbar').css("display","block");
+	$('.progressbar').animate({opacity:1},2000
+
+	);
+
 	var files = document.getElementById('my-file');
 	for (var i = 0; i < files.files.length; i++) {
 		var file = files.files[i];
@@ -13,7 +19,14 @@ function upload() {
 
 		var pdfText = new PDFText();
 		pdfText.get(tmppath, function (text) {
-			document.getElementById('content').innerHTML = '<p>' + text + '</p>';
+			document.getElementById('content').innerHTML = '<p style="color: #616161">' + text + '</p>';
+			console.log('success');
+			console.log(text);
+			$('.progressbar').animate({opacity:0},2000,function () {
+				$('.progressbar').css("display","none");
+				$( "#contentpanel" ).animate({opacity:1}, 2000);
+			});
+
 		});
 
 
